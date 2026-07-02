eb0d230abc7869e026afa782734d0db5e11be758
Guillaume Tucker <gtucker@gtucker.io>
2026-07-02 14:50:18 +0200

    WIP use pcap crate

--- src/svgen.rs
+++ src/svgen.rs
@@ -3,8 +3,11 @@
 
 
 // ToDo: read packets sent from the pcap using iec6150 crate + network socket
+// ToDo: sudo apt install libpcap-dev
 
 use std;
+use libc;
+use pcap::{Capture, Linktype, Packet, PacketHeader};
 
 struct Generator {
     _f: f32,
@@ -42,7 +45,17 @@ impl Generator {
 
 pub fn hello() {
     let mut gen = make50hz(Phase::Ph0);
-    while gen.t < 3.0 {
+    let mut cap = Capture::dead(Linktype::ETHERNET).unwrap();
+    let hdr = PacketHeader {
+        ts: libc::timeval { tv_sec: 1, tv_usec: 0 },
+        caplen: 2,
+        len: 2,
+    };
+    let data = vec![0x34, 0x12];
+    let pkt = Packet {header: &hdr, data: &data};
+    let mut dump = cap.savefile("dump.pcap").unwrap();
+    dump.write(&pkt);
+    while gen.t < 0.01 /*3.0*/ {
         let value = gen.gen();
         println!("value({}): {}", gen.t, value);
         gen.step();
