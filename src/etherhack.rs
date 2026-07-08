ea28785eb99132e0004eb4a4114e32172c7bf0e0
Guillaume Tucker <gtucker@gtucker.io>
2026-07-08 13:43:55 +0200

    WIP src/etherhack.rs

--- src/etherhack.rs
+++ src/etherhack.rs
@@ -1,24 +1,43 @@
 // SPDX-License-Identifier: MIT
 // Copyright (C) 2026 Guillaume Tucker
 
+// cargo build && sudo target/debug/iec_61850_hack
+
 use pnet;
 use pnet::datalink::{self, NetworkInterface};
 use pnet::datalink::Channel::Ethernet;
+use pnet::packet::ethernet::EthernetPacket;
+use pnet::packet::Packet;
 
-pub fn sink() {
+pub fn sink() -> Vec<u8> {
     println!("Starting Ethernet sink...");
-    // let iface_name = "lo";
-    let iface_name = "wlp2s0";
+    let iface_name = "lo";
+    // let iface_name = "wlp2s0";
+    // let iface_name = "enp7s0";
     let interface_names_match =
         |iface: &NetworkInterface| iface.name == iface_name;
     let interfaces = datalink::interfaces();
-    let interface = interfaces
+    let interface =
+        interfaces
         .into_iter()
         .filter(interface_names_match)
         .next()
         .unwrap();
+
+    println!("Interface: {0}", interface.name);
+
+    let (mut _tx, mut rx) = match datalink::channel(
+        &interface, Default::default()) {
+        Ok(Ethernet(_tx, rx)) => (_tx, rx),
+        Ok(_) => panic!(
+            "Unhandled channel type"
+        ),
+        Err(e) => panic!(
+            "An error occurred when creating the datalink channel: {}", e
+        )
+    };
+
     if let Some(mac) = interface.mac {
-        println!("Interface: {0}", interface.name);
         let macaddr = mac.octets();
         print!("MAC address: ");
         println!("{:02x}:{:02x}:{:02x}:{:02x}:{:02x}:{:02x}",
@@ -27,49 +46,20 @@ pub fn sink() {
         );
     }
 
-    // ToDo: figure out how to use any MAC and not just Ethernet?
-    // ToDo: also check the bittwist / bpftrace hack works on the same iface
-    let (mut tx, mut rx) = match datalink::channel(
-        &interface, Default::default()) {
-        Ok(Ethernet(tx, rx)) => (tx, rx),
-        Ok(_) => panic!("Unhandled channel type"),
-        Err(e) => panic!("An error occurred when creating the datalink channel: {}", e)
-};
-
-    //let (mut tx, mut rx) = datalink::channel(&interface, Default::default()).unwrap();
-    let chan = datalink::channel(&interface, Default::default());
-
-/*
     loop {
         match rx.next() {
             Ok(packet) => {
                 let packet = EthernetPacket::new(packet).unwrap();
-
-                // Constructs a single packet, the same length as the one received,
-                // using the provided closure. This allows the packet to be constructed
-                // directly in the write buffer, without copying. If copying is not a
-                // problem, you could also use send_to.
-                //
-                // The packet is sent once the closure has finished executing.
-                tx.build_and_send(1, packet.packet().len(),
-                    &mut |mut new_packet| {
-                        let mut new_packet = MutableEthernetPacket::new(new_packet).unwrap();
-
-                        // Create a clone of the original packet
-                        new_packet.clone_from(&packet);
-
-                        // Switch the source and destination
-                        new_packet.set_source(packet.get_destination());
-                        new_packet.set_destination(packet.get_source());
-                });
+                let ethtype = packet.get_ethertype().0;
+                if ethtype == 0x88ba {
+                    // ToDo: callback to handle the packet or iterator or pipe
+                    return packet.packet().to_vec();
+                }
             },
             Err(e) => {
                 // If an error occurs, we can handle it here
                 panic!("An error occurred while reading: {}", e);
             }
         }
-}
-    */
-
-    println!("Done.");
+    }
 }
